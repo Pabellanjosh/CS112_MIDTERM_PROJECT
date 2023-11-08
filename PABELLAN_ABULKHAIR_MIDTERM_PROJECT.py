7e94e4e5ee586bbb8c9e233ec56397c1e6bae086
Pabellanjosh <145018260+Pabellanjosh@users.noreply.github.com>
2023-11-08 20:16:00 +0800

    Add files via upload

---
+++ PABELLAN_ABULKHAIR_MIDTERM_PROJECT.py
@@ -0,0 +1,20 @@
+# Variables
+x = 5
+y = 10
+
+# User Input
+user_input = int(input("Enter a number: "))
+
+# Operators
+result = x + y - user_input
+
+# If-else selections
+if result > 0:
+    print("The result is positive.")
+elif result < 0:
+    print("The result is negative.")
+else:
+    print("The result is zero.")
+
+# Functional and error-free
+print("This code has been executed without any errors.")
